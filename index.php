1783522ea7c11347524ca85d30b0218b162b7b94
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-02-16 15:41:38 +0100

    inizio

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Google Faq</title>
+</head>
+<body>
+    <header>
+        <div class='container_header'>
+            <div class='container_col'>
+                <img src="https://1000marche.net/wp-content/uploads/2020/03/Google-logo.png" alt="">
+                <span class='privacy_termini'>Privacy e termini</span>
+            </div>
+
+        </div>
+    </header>
+    
+</body>
+</html>
