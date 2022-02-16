e233b193c7435cdf2c7e96550b1b4d9e520bfbd2
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-02-16 15:53:56 +0100

    header

--- index.php
+++ index.php
@@ -11,8 +11,21 @@
     <header>
         <div class='container_header'>
             <div class='container_col'>
-                <img src="https://1000marche.net/wp-content/uploads/2020/03/Google-logo.png" alt="">
-                <span class='privacy_termini'>Privacy e termini</span>
+                <div class='container_logo'>
+                    <img src="https://1000marche.net/wp-content/uploads/2020/03/Google-logo.png" alt="">
+                    <span class='privacy_termini'>Privacy e termini</span>
+                </div>
+            </div>
+
+            <div class='container_navbar'>
+                <ul>
+                    <li><a href="#">Introduzione</a></li>
+                    <li><a href="#">Norme sulla privacy</a></li>
+                    <li><a href="#">Termini di servizio</a></li>
+                    <li><a href="#">Tecnologie</a></li>
+                    <li><a href="#">Domande frequenti</a></li>
+                </ul>
+
             </div>
 
         </div>
